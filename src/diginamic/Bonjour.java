d6c51fa057bf699b9548e62d828e5fcaf4ff2558
rbonnamy <rbonnamy@diginamic.fr>
2022-10-06 15:36:01 +0200

    Init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,15 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		
+		int i=1; // acte de naissance
+		
+		i=i+1; // Java exécute de droite à gauche quand il y a un =
+		
+		int monAgeMonJardin = 9;
+		
+	}
+
+}
